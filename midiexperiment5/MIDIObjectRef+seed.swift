647cc7a40588bd5606fb4c3ee91fb6257f1ebb4b
Jose Vazquez <jose@NubeGris.home>
2017-03-04 11:05:55 -0500

    Initial Commit

---
+++ midiexperiment5/MIDIObjectRef+seed.swift
@@ -0,0 +1,9 @@
+//
+//  MIDIObjectRef+seed.swift
+//  midiexperiment5
+//
+//  Created by Jose Vazquez on 2/28/17.
+//  Copyright © 2017 Jose Vazquez. All rights reserved.
+//
+
+import Foundation
---
+++ midiexperiment5/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  midiexperiment5
+//
+//  Created by Jose Vazquez on 2/21/17.
+//  Copyright © 2017 Jose Vazquez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
